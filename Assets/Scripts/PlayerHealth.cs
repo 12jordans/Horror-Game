6e1c6c0ceb1a508b4aca7efc4953846a25a00710
Your name or nickname here <jordanshandssparks@gmail.com>
2022-11-07 13:15:26 -0800

    Add Ghost

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -8,22 +8,31 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 3;
     public int currentHealth;
     public HealthBar healthBar;
+    public FollowPlayer followPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        followPlayer._index = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Take damage
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        /*
+        if (followPlayer._index == 3)
+        {
+            Debug.Log(followPlayer._index);
             currentHealth -= 1;
             healthBar.SetHealth(currentHealth);
+            followPlayer._index = 0;
+            Debug.Log(followPlayer._index);
         }
+        */
+
     }
 
     private void OnTriggerEnter(Collider other) {
